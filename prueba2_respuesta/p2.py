469f8dc1e83a043497a86c34f4e0c3b0776450bb
caun94 <cristobalugarte6@gmail.com>
2026-06-18 12:15:54 -0400

    clase 12

--- prueba2_respuesta/p2.py
+++ prueba2_respuesta/p2.py
@@ -1,7 +1,6 @@
 n = 3
 while n < 5:
     n = int(input("Ingrese un número entero mayor o igual a 5: "))  
-
 ind = int(n*(2/3))
 
 for i in range(n):
@@ -9,7 +8,6 @@ for i in range(n):
         # Diagonal desde ind, 0 hasta 0, ind en la otra direccion 
         if (i == ind - j and j <= ind)  or i == ind or j == ind:
             print('*', end=' ')
-
         else:
             print(' ', end=' ')
     print()
